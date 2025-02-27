93eaa9fc07c4d7683e420a6a46b2547c27239e53
Eina Safor Coma Bages <ecoma-ba@student.42barcelona.com>
2025-02-27 15:11:52 +0100

    detecció correcta de parets en els 4 costats

--- src/graphics/render.c
+++ src/graphics/render.c
@@ -10,20 +10,20 @@ void	find_next_edge(t_game *game, t_vec2 *current)
 
 	// finds the closest edge in each direction.
 	if (cosf(game->player.dir) > 0)
-		dest.i = ceilf(current->i);
+		dest.i = floorf(current->i + 1);
 	else
-		dest.i = floorf(current->i);
+		dest.i = ceilf(current->i - 1);
 	if (sinf(game->player.dir) > 0)
-		dest.j = ceilf(current->j);
+		dest.j = floorf(current->j + 1);
 	else
-		dest.j = floorf(current->j);
+		dest.j = ceilf(current->j - 1);
 	// calculates how far away we are from each edge.
 	if (!cosf(game->player.dir))
-		steps.i = 2;
+		steps.i = 10000;
 	else
 		steps.i = (dest.i - current->i) / cosf(game->player.dir);
 	if (!sinf(game->player.dir))
-		steps.j = 2;
+		steps.j = 10000;
 	else
 		steps.j = (dest.j - current->j) / sinf(game->player.dir);
 	// update the current position
@@ -32,17 +32,32 @@ void	find_next_edge(t_game *game, t_vec2 *current)
 	printf("Found edge: (%f, %f)\n", current->i, current->j);
 }
 
+char	get_map_element(t_game *game, t_vec2 *point, float angle)
+{
+	int	x;
+	int	y;
+
+	x = floorf(point->i);
+	y = floorf(point->j);
+	if (cosf(angle) < 0 && (float)x == point->i)
+		x--;
+	if (sinf(angle) < 0 && (float)y == point->j)
+		y--;
+	return (game->map[x][y]);
+}
+
 void	raycast(t_game *game, int x)
 {
 	t_vec2	ray;
 	t_vec2	next_edge;
+	int		count;
 
 	ray = (t_vec2){game->player.pos.i, game->player.pos.j};
 	find_next_edge(game, &ray);
-//	while (game->map[(int)ray.i][(int)ray.j] != '1' && ray.i > -1 && ray.j > -1
-//		&& ray.i < 6 && ray.j < 6)
+	while (get_map_element(game, &ray, game->player.dir) != '1')
 		find_next_edge(game, &ray);
 	printf("Next edge: (%f, %f)\n", ray.i, ray.j);
+	mlx_put_pixel(game->img, ray.i * WIDTH / 7, ray.j * HEIGHT / 7, 0xFF0000FF);
 }
 
 void	render_scene(t_game *game)
--- src/main.c
+++ src/main.c
@@ -34,43 +34,70 @@ void	ft_mov_key_hook(mlx_key_data_t keydata, t_game *game)
 	}
 	printf("Player position: (%f, %f)\n", game->player.pos.i,
 		game->player.pos.j);
+	mlx_put_pixel(game->img, game->player.pos.i * WIDTH / 7, game->player.pos.j
+		* HEIGHT / 7, 0xFF00FF);
 }
 
 void	ft_rot_key_hook(mlx_key_data_t keydata, t_game *game)
 {
 	float	rot_speed;
 
-	rot_speed = M_PI / 100;
+	rot_speed = M_PI / 360;
 	if (keydata.key == MLX_KEY_LEFT)
 		game->player.dir -= rot_speed;
 	else if (keydata.key == MLX_KEY_RIGHT)
 		game->player.dir += rot_speed;
-	printf("Player direction: %F\n", game->player.dir);
+	printf("Player direction: %F\n", game->player.dir * 180 / M_PI);
 }
 
 void	ft_key_hook(mlx_key_data_t keydata, t_game *game)
 {
 	printf("key: %d\n", keydata.key);
-	if (ft_isascii(keydata.key))
+	if (keydata.key == MLX_KEY_SPACE)
+		paintmap(game);
+	else if (ft_isascii(keydata.key))
 		ft_mov_key_hook(keydata, game);
 	else
 		ft_rot_key_hook(keydata, game);
 	render_scene(game);
 }
 
+void	paintmap(t_game *game)
+{
+	int	i;
+	int	j;
+
+	i = -1;
+	while (++i < WIDTH)
+	{
+		j = -1;
+		while (++j < HEIGHT)
+		{
+			if (game->map[i * 7 / WIDTH][j * 7 / HEIGHT] == '0')
+				mlx_put_pixel(game->img, i, j, 0xFF);
+			else
+				mlx_put_pixel(game->img, i, j, 0xFFFFFFFF);
+		}
+	}
+	mlx_put_pixel(game->img, game->player.pos.i * WIDTH / 7, game->player.pos.j
+		* HEIGHT / 7, 0xFF00FF);
+}
+
 int32_t	main(void)
 {
 	t_game	game;
 
-	game.map = (char *[6]){"11111", "10001", "10101", "10001", "11111", NULL};
+	game.map = (char *[8]){"1111111", "1000001", "1000001", "1001001",
+		"1000001", "1000001", "1111111", NULL};
 	game.player.pos = (t_vec2){1.3, 1.2};
-	game.player.dir = M_PI_2;
+	game.player.dir = M_PI / 3;
 	game.mlx = mlx_init(WIDTH, HEIGHT, "cub3d", false);
 	if (!game.mlx)
 		ft_error();
 	game.img = mlx_new_image(game.mlx, WIDTH, HEIGHT);
 	if (!game.img || (mlx_image_to_window(game.mlx, game.img, 0, 0) < 0))
 		ft_error();
+	paintmap(&game);
 	mlx_key_hook(game.mlx, (mlx_keyfunc)ft_key_hook, &game);
 	mlx_loop(game.mlx);
 	mlx_terminate(game.mlx);
