340a66fa49b43ca66424a1406910b956f1b73974
eina <109956262+encarbassotnopot@users.noreply.github.com>
2025-03-11 13:51:31 +0100

    pintem de fitxers. problema: no agafo bé l'alçada (i crec q tampoc els colors segons com)

--- include/render.h
+++ include/render.h
@@ -1,7 +1,7 @@
 #ifndef RENDER_H
 # define RENDER_H
 
-float angle_from_x(int x);
-void draw_oob(t_game *game, int x);
-void draw_wall(t_game *game, float dist, int x, char face);
+float	angle_from_x(int x);
+void	draw_oob(t_game *game, int x);
+void	draw_wall(t_game *game, t_vec2 *ray, int x, char face);
 #endif
